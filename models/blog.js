af1c67ad6f73eea04ec51d4c12edc385cd29584c
nikhil <nikhilcares22@gmail.com>
2020-10-20 11:33:26 +0530

    remove the database url/make separate dbs for dev/prod

--- models/blog.js
+++ models/blog.js
@@ -1,9 +1,6 @@
 let mongoose = require('mongoose');
 
 const { url } = require('../config.js');
-console.log(url)
-
-//"mongodb+srv://nikhil:nehminilu@cluster0.udzon.mongodb.net/restful_blog_app?retryWrites=true&w=majority"//
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 //MONGOOSE MODEL CONFIG
